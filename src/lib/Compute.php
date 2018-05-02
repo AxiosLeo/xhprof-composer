f8a8f58f01f36ea6ba6a7453699d070dd0e3d218
axios <axioscros@aliyun.com>
2018-05-02 17:15:36 +0800

    数据计算类

---
+++ src/lib/Compute.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/5/2 14:43
+ */
+
+namespace xhprof\lib;
+
+
+class Compute
+{
+    public static function xhprof_compute_flat_info($raw_data, &$overall_totals){
+
+    }
+}
