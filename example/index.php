189632cfe59e8313f91ab0bd4be198c1c9c10537
axios <axioscros@aliyun.com>
2018-05-02 14:27:46 +0800

    update example

--- example/index.php
+++ example/index.php
@@ -12,8 +12,28 @@ namespace xhprof;
 require_once __DIR__.'/../vendor/autoload.php';
 
 XHProfDebug::start();
+
+/*** begin ***/
+
 dump('this is example for use xhprof-composer');
 $a = pow(2,10);
 dump($a);
+
+/*** end ***/
+
 $runs = XHProfDebug::end();
-dump($runs->getData());
+
+
+// Query Data
+$run_id = $runs->getRunId();
+dump($run_id);
+dump(XHProfRuns::query()->find($run_id));
+
+$list = [
+    '38cece5b1ce049e446b5f58fba0aba7c',
+    '86cd5712d2a679efe80c743d1d2342e8',
+    '773a5a2a3a50ebf16fa0dc5256cafd46'
+];
+
+$data = XHProfRuns::query()->select($list);
+dump($data);
