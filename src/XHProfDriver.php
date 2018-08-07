8e21afab5d54b2b4a8f24c8870e528c2e73d0d4e
axios <axioscros@aliyun.com>
2018-08-07 16:39:59 +0800

    驱动类

---
+++ src/XHProfDriver.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author  : axios
+ * @email   : axiosleo@foxmail.com
+ * @blog    : http://hanxv.cn
+ * @datetime: 2018/8/7 15:28
+ */
+
+namespace xhprof;
+
+abstract class XHProfDriver
+{
+    private static $instance;
+
+    /**
+     * @return mixed
+     */
+    public static function instance()
+    {
+        if (is_null(self::$instance)) {
+            $driver = XHProf::options()->get('driver', 'file');
+            $driver = "xhprof\\driver\\" . ucfirst(strtolower($driver));
+
+            self::$instance = new $driver();
+        }
+        return self::$instance;
+    }
+
+    protected $options;
+
+    abstract public function save($run_id, $data);
+
+    abstract public function get($run_id);
+}
